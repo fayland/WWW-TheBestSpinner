49dda2572c477d86e266ad220a7d5bd1034e9a14
Fayland Lam <fayland@gmail.com>
2013-12-18 17:59:38 +0800

    with milla new WWW::TheBestSpinner

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+WWW::TheBestSpinner - Blah blah blah
+
+# SYNOPSIS
+
+    use WWW::TheBestSpinner;
+
+# DESCRIPTION
+
+WWW::TheBestSpinner is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2013- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/WWW/TheBestSpinner.pm
@@ -0,0 +1,39 @@
+package WWW::TheBestSpinner;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::TheBestSpinner - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use WWW::TheBestSpinner;
+
+=head1 DESCRIPTION
+
+WWW::TheBestSpinner is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
